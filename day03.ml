934077439c3765f7e9f41572895a06a1f6c1df63
francisco souza <fsouza@users.noreply.github.com>
2020-12-03 00:18:50 -0500

    Day03 part1

---
+++ day03.ml
@@ -0,0 +1,27 @@
+open StdLabels
+
+let read_input () =
+  let rec read_input acc =
+    try
+      let line = read_line () in
+      read_input (line :: acc)
+    with End_of_file -> acc |> List.rev |> Array.of_list
+  in
+  read_input []
+
+let count_trees grid =
+  let cols = String.length grid.(0) in
+  let is_tree = Char.equal '#' in
+  let rec loop row col acc =
+    if row = Array.length grid then acc
+    else
+      let curr_row = grid.(row) in
+      let chr = curr_row.[col] in
+      let new_acc = if is_tree chr then acc + 1 else acc in
+      loop (row + 1) ((col + 3) mod cols) new_acc
+  in
+  loop 0 0 0
+
+let part01 input = input |> count_trees |> string_of_int |> print_endline
+
+let () = read_input () |> part01

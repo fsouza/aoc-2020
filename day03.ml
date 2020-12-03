8b3cc7997e35c33f0fee6a0564184bce579f5607
francisco souza <fsouza@users.noreply.github.com>
2020-12-03 00:23:15 -0500

    Day03 part2

--- day03.ml
+++ day03.ml
@@ -9,19 +9,27 @@ let read_input () =
   in
   read_input []
 
-let count_trees grid =
+let count_trees ~right ~down grid =
   let cols = String.length grid.(0) in
   let is_tree = Char.equal '#' in
   let rec loop row col acc =
-    if row = Array.length grid then acc
+    if row >= Array.length grid then acc
     else
       let curr_row = grid.(row) in
       let chr = curr_row.[col] in
       let new_acc = if is_tree chr then acc + 1 else acc in
-      loop (row + 1) ((col + 3) mod cols) new_acc
+      loop (row + down) ((col + right) mod cols) new_acc
   in
   loop 0 0 0
 
-let part01 input = input |> count_trees |> string_of_int |> print_endline
+let part01 input =
+  input |> count_trees ~right:3 ~down:1 |> string_of_int |> print_endline
 
-let () = read_input () |> part01
+let part02 input =
+  let answer_p1 = 153 in
+  [ (1, 1); (5, 1); (7, 1); (1, 2) ]
+  |> List.map ~f:(fun (right, down) -> input |> count_trees ~right ~down)
+  |> List.fold_left ~init:answer_p1 ~f:(fun acc v -> acc * v)
+  |> string_of_int |> print_endline
+
+let () = read_input () |> part02

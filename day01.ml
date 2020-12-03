3f2f134c0fcf5681344d8d1af2117b327b5db5fb
francisco souza <fsouza@users.noreply.github.com>
2020-12-02 22:59:09 -0500

    Prep for improving day01
    But I'll work on day02 first.

--- day01.ml
+++ day01.ml
@@ -217,16 +217,16 @@ let find_pair ?(left = 0) input target =
   in
   loop left (Array.length input - 1)
 
-let part01 () =
+let part01 input =
   find_pair input 2020
   |> Option.map (fun (x, y) -> x * y)
   |> Option.value ~default:0 |> string_of_int |> print_endline
 
-let part02 () =
+let part02 input =
   input
   |> Array.iteri ~f:(fun i elm ->
          match find_pair ~left:(i + 1) input (2020 - elm) with
          | Some (x, y) -> elm * x * y |> string_of_int |> print_endline
          | None -> ())
 
-let () = part02 ()
+let () = part02 input

152c44250da83750b50b4226d98b834e9c70f38a
francisco souza <fsouza@users.noreply.github.com>
2020-12-06 00:16:12 -0500

    day06 part2

--- day06.ml
+++ day06.ml
@@ -4,9 +4,16 @@ module CharSet = Set.Make (Char)
 let parse_group lines =
   lines |> String.concat ~sep:"" |> String.to_seq |> CharSet.of_seq
 
+let parse_group_p2 lines =
+  match
+    lines |> List.map ~f:(fun line -> String.to_seq line |> CharSet.of_seq)
+  with
+  | [] -> CharSet.empty
+  | hd :: tl -> List.fold_left ~f:CharSet.inter ~init:hd tl
+
 let read_input () =
   let rec read_input groups curr_lines =
-    let add_group () = read_input (parse_group curr_lines :: groups) [] in
+    let add_group () = read_input (parse_group_p2 curr_lines :: groups) [] in
     try
       let line = read_line () in
       if line = "" then add_group () else read_input groups (line :: curr_lines)
@@ -15,8 +22,8 @@ let read_input () =
   in
   read_input [] []
 
-let part1 () =
+let part2 () =
   read_input ()
   |> List.fold_left ~f:(fun acc set -> acc + CharSet.cardinal set) ~init:0
 
-let () = part1 () |> string_of_int |> print_endline
+let () = part2 () |> string_of_int |> print_endline

0399628de97be9cb306f3bb6ef84a7e0ade7413e
francisco souza <fsouza@users.noreply.github.com>
2020-12-13 17:18:51 -0500

    day13 part2 without brute force
    Had to study a bit x)

--- day13/part2.ml
+++ day13/part2.ml
@@ -23,6 +23,7 @@ let wait_time n m =
   let diff = n mod m in
   if diff = 0 then diff else m - diff
 
+(* this was the original brute force implementation *)
 let rec look_for_answer offset_buses =
   let delta =
     match offset_buses with
@@ -37,7 +38,21 @@ let rec look_for_answer offset_buses =
   in
   find_answer delta offset_buses
 
+(* this is a better solution, "inspired" by @lizthegrey's solution :D
+ *
+ * This is somewhat similar to the brute-force solution, but instead of always
+ * adding the initial offset to the candidate ts, it keeps track of a minimal
+ * value that keeps the timestamp divisible by all previous timestamps and the
+ * current one (taking the offset into consideration).
+ *
+ * *)
+let rec find_answer min_val running_product = function
+  | [] -> min_val
+  | (offset, bus) :: tl when (min_val + offset) mod bus = 0 ->
+      find_answer min_val (running_product * bus) tl
+  | l -> find_answer (min_val + running_product) running_product l
+
 let () =
   let input = read_input () in
   let _, offset_buses = parse_input input in
-  look_for_answer offset_buses |> string_of_int |> print_endline
+  find_answer 0 1 offset_buses |> string_of_int |> print_endline

bea6083fcf774807126ec1d7bcfefbc530a15182
francisco souza <fsouza@users.noreply.github.com>
2020-12-14 23:10:46 -0500

    day14 part2 the lame way
    So instead of constructing the mask with bitwise operators, I build them
    with an array. I suck.

--- day14/part2.ml
+++ day14/part2.ml
@@ -17,8 +17,17 @@ let read_input () =
 
 let print_arr = Array.iter ~f:(Printf.printf "%d\n")
 
-let make_floating_masks length floating =
-  let floating = floating |> List.map ~f:(( - ) length) |> Array.of_list in
+let p2 v = 2. ** float_of_int v |> int_of_float
+
+let convert arr =
+  let length = Array.length arr in
+  let rec convert' acc idx =
+    if idx = length then acc
+    else convert' (acc + (arr.(idx) * p2 (length - 1 - idx))) (idx + 1)
+  in
+  convert' 0 0
+
+let make_floating_masks mask_length floating =
   let vs = Array.make (Array.length floating) 0 in
   let length = Array.length vs in
   let incr_vs () =
@@ -35,18 +44,12 @@ let make_floating_masks length floating =
     (* this isn't quite right as it produces the mask from 000 twice, but for
      * this problem, it's not an issue, since it's just gonna write to the
      * memory position twice *)
-    let mask_0 = ref 0 in
-    let mask_1 = ref 0 in
+    let mask_0 = Array.make mask_length 0 in
+    let mask_1 = Array.make mask_length 0 in
     Array.iter2
-      ~f:(fun pos v ->
-        if v = 0 then (
-          mask_0 := (!mask_0 lor 1) lsl (pos - 1);
-          mask_1 := !mask_1 lsl (pos - 1))
-        else (
-          mask_0 := !mask_0 lsl (pos - 1);
-          mask_1 := (!mask_1 lor 1) lsl (pos - 1)))
+      ~f:(fun pos v -> if v = 0 then mask_0.(pos) <- 1 else mask_1.(pos) <- 1)
       floating vs;
-    (lnot !mask_0, !mask_1)
+    (mask_0 |> convert |> lnot, mask_1 |> convert)
   in
   let rec make_masks acc stop =
     if stop then acc else make_masks (make_mask_from_vs () :: acc) (incr_vs ())
@@ -56,7 +59,9 @@ let make_floating_masks length floating =
 let parse_mask mask =
   let length = String.length mask in
   let rec parse_mask' mask floating = function
-    | [] -> (mask, floating |> List.rev |> make_floating_masks length)
+    | [] ->
+        ( mask,
+          floating |> List.rev |> Array.of_list |> make_floating_masks length )
     | (_, '0') :: tl -> parse_mask' (mask lsl 1) floating tl
     | (_, '1') :: tl -> parse_mask' ((mask lsl 1) lor 1) floating tl
     | (i, 'X') :: tl -> parse_mask' (mask lsl 1) (i :: floating) tl

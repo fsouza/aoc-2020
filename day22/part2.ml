bc1f27b2483c8c1ef59bdb8f355081b49b4f8925
francisco souza <fsouza@users.noreply.github.com>
2020-12-22 01:21:09 -0500

    day22 part2 with a hashtbl instead of a set

--- day22/part2.ml
+++ day22/part2.ml
@@ -1,14 +1,6 @@
 open StdLabels
 open MoreLabels
 
-module S = Set.Make (struct
-  type t = int * int
-
-  let compare ((p1_1, p2_1) : t) ((p1_2, p2_2) : t) : int =
-    let p1_compare = Int.compare p1_1 p1_2 in
-    if p1_compare <> 0 then p1_compare else Int.compare p2_1 p2_2
-end)
-
 (* representing the players as a list isn't great in terms of performance, but
  * I don't want to implement a doubly-linked list *)
 let parse_player input =
@@ -35,40 +27,37 @@ let read_input () =
   in
   read_input []
 
-type winner = Left of int | Right of int
-
-let hash player =
-  let rec hash' acc multiplier = function
-    | [] -> acc
-    | hd :: tl -> hash' (acc + (hd * multiplier)) (multiplier - 1) tl
-  in
-  hash' 0 (List.length player) player
+type winner = Left of int list | Right of int list
 
 let rec play_game players =
-  let rec play_round rounds (p1, p2) =
-    let h1 = hash p1 in
-    let h2 = hash p2 in
-    if S.mem (h1, h2) rounds then Left h1
-    else
+  let rounds = Hashtbl.create 1 in
+  let rec play_round (p1, p2) =
+    if Hashtbl.mem rounds (p1, p2) then Left p1
+    else (
+      Hashtbl.replace ~key:(p1, p2) ~data:true rounds;
       match (p1, p2) with
-      | [], p2 -> Right h2
-      | p1, [] -> Left h1
+      | [], p2 -> Right p2
+      | p1, [] -> Left p1
       | c1 :: tl1, c2 :: tl2 ->
           if c1 <= List.length tl1 && c2 <= List.length tl2 then
             match play_game (tl1, tl2) with
-            | Left _ ->
-                play_round (S.add (h1, h2) rounds) (tl1 @ [ c1; c2 ], tl2)
-            | Right _ ->
-                play_round (S.add (h1, h2) rounds) (tl1, tl2 @ [ c2; c1 ])
-          else if c1 > c2 then
-            play_round (S.add (h1, h2) rounds) (tl1 @ [ c1; c2 ], tl2)
-          else play_round (S.add (h1, h2) rounds) (tl1, tl2 @ [ c2; c1 ])
+            | Left _ -> play_round (tl1 @ [ c1; c2 ], tl2)
+            | Right _ -> play_round (tl1, tl2 @ [ c2; c1 ])
+          else if c1 > c2 then play_round (tl1 @ [ c1; c2 ], tl2)
+          else play_round (tl1, tl2 @ [ c2; c1 ]))
   in
-  play_round S.empty players
+  play_round players
 
 let score winner =
-  match winner with
-  | Left p -> p
-  | Right p -> p
+  let player =
+    match winner with
+    | Left p -> p
+    | Right p -> p
+  in
+  let rec score' acc multiplier = function
+    | [] -> acc
+    | hd :: tl -> score' (acc + (hd * multiplier)) (multiplier - 1) tl
+  in
+  score' 0 (List.length player) player
 
 let () = read_input () |> play_game |> score |> Printf.printf "%d\n"

2311af6263adec64d0c8cd680470fa712097d3ab
francisco souza <fsouza@users.noreply.github.com>
2020-12-24 01:05:47 -0500

    day24: unify part1 and part2
    part2 is actually an extra step on top of part1, so we can do this.

--- day24/day24.ml
+++ day24/day24.ml
@@ -61,8 +61,51 @@ let process_line black_tiles instructions =
   if S.mem coordinates black_tiles then S.remove coordinates black_tiles
   else S.add coordinates black_tiles
 
+let get_adjacent (i, j) =
+  [ East; Southeast; Southwest; West; Northwest; Northeast ]
+  |> List.map ~f:offset
+  |> List.map ~f:(fun (offset_i, offset_j) -> (i + offset_i, j + offset_j))
+
+let count_black_adjacents black_tiles coords =
+  get_adjacent coords |> S.of_list |> S.inter black_tiles |> S.cardinal
+
+let get_white_tiles black_tiles =
+  black_tiles
+  |> S.fold ~init:S.empty ~f:(fun coords white_tiles ->
+         let all_adjacent = coords |> get_adjacent |> S.of_list in
+         S.union white_tiles (S.diff all_adjacent black_tiles))
+
+let run_interation black_tiles white_tiles =
+  S.union black_tiles white_tiles
+  |> S.fold ~init:S.empty ~f:(fun coords s ->
+         let black_adjacents = count_black_adjacents black_tiles coords in
+         if
+           S.mem coords black_tiles
+           && black_adjacents > 0
+           && black_adjacents < 3
+         then S.add coords s
+         else if S.mem coords white_tiles && black_adjacents = 2 then
+           S.add coords s
+         else s)
+
+let rec flip_n_times n black_tiles =
+  if n = 0 then black_tiles
+  else
+    flip_n_times (n - 1)
+      (run_interation black_tiles (get_white_tiles black_tiles))
+
+let part1 = Fun.id
+
+let part2 = flip_n_times 100
+
 let () =
+  let run_part2 = ref false in
+  Arg.parse_argv Sys.argv
+    [ ("-part2", Arg.Set run_part2, "run part 2") ]
+    (Fun.const ()) "Advent of Code Day 24";
+  let process_fn = if !run_part2 then part2 else part1 in
   read_input ()
   |> List.fold_left ~init:S.empty ~f:process_line
+  |> process_fn
   |> S.cardinal
   |> Printf.printf "%d\n"
